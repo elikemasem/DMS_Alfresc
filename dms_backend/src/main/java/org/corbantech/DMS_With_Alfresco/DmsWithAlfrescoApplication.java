0d1c5c6102822c5c32834112538afb3a1ca1b545
ElikemAg <elikemag@stlghana.com>
2025-03-21 10:55:16 +0000

    First Commit To development

---
+++ dms_backend/src/main/java/org/corbantech/DMS_With_Alfresco/DmsWithAlfrescoApplication.java
@@ -0,0 +1,13 @@
+package org.corbantech.DMS_With_Alfresco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsWithAlfrescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsWithAlfrescoApplication.class, args);
+	}
+
+}
---
+++ dms_backend/src/test/java/org/corbantech/DMS_With_Alfresco/DmsWithAlfrescoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.corbantech.DMS_With_Alfresco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsWithAlfrescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
